b7b57fd9a6dee11eb82c3d84d88878e0d5e4af26
Wellington Siqueira <wellingtonsiqueira.santos@gmail.com>
2023-05-21 14:35:37 -0300

    Iniciando o js

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,28 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
 
 function styles(){
     return gulp.src('./src/styles/*.scss')
-    .pipe(sass({ outputStyle: 'compressed'}))
-    .pipe(gulp.dest('./dist/style/'));
+        .pipe(sass({ outputStyle: 'compressed'}))
+        .pipe(gulp.dest('./dist/style/'));
 }
 
 function images(){
     return gulp.src('./src/images/**/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('./dist/images'));
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default = gulp.parallel(styles, images)
+function comprimirjs() {
+    return gulp.src('src/js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js/'));
+}
+
+exports.default = gulp.parallel(styles, images, comprimirjs)
 
 exports.watch = function(){
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles, comprimirjs))
 }
---
+++ src/js/main.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const btnsecoes = document.querySelectorAll('.btnsecoes');
+    const secaoContainer = document.querySelectorAll('.btncontainersecao');
+
+    for (let i = 0; i < btnsecoes.length; i++) {
+        btnsecoes[i].addEventListener('click', function(botao){
+            const secaoAlvo = botao.target.dataset.tabButton;
+            const secaoSelecionada = botao.target
+
+            for (let indice = 0; indice < secaoContainer.length; indice++) {
+                const secaoItem = secaoContainer[indice];
+                const nomeId = secaoItem.getAttribute('id');
+                const dataSetID = secaoItem.dataset.tabId;
+
+                if (dataSetID == secaoAlvo) {
+                    secaoItem.style.display = "block";
+                    secaoSelecionada.style.borderBottom = "3px solid white";
+                } else {
+                    secaoItem.style.display = "none";
+                    const btnsecoesItem = document.querySelectorAll('.btnsecoes');
+
+                    for (let posicao = 0; posicao < btnsecoesItem.length; posicao++) {
+                        const item = btnsecoesItem[posicao]
+
+                        console.log(item.dataset.tabButton, secaoAlvo)
+                        if (item.dataset.tabButton !== secaoAlvo) {
+                            item.style.borderBottom = "3px solid transparent";
+                        }
+                    }  
+                }
+            } 
+        })
+    }
+
+       
+      
+    
+})
